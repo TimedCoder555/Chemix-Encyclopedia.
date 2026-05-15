93881b9579407fed5e3d53164670d3ddd2aa4943
TimedCoder555 <matrixebay0110@gmail.com>
2026-05-15 08:14:28 +0600

    Create setupTests.js

---
+++ src/setupTests.js
@@ -0,0 +1,21 @@
+// Chemix-Encyclopedia Test Environment ⚗️
+
+/*
+  jest-dom adds custom matchers for testing React components.
+  Example:
+  expect(element).toHaveTextContent(/Chemix/i)
+*/
+
+import "@testing-library/jest-dom";
+
+/* Console Welcome */
+
+console.log(
+  "%c Chemix-Encyclopedia Testing System Ready 🧪",
+  "color: #00e5ff; font-size: 16px; font-weight: bold;"
+);
+
+console.log(
+  "%c Developed by timedcoder",
+  "color: #7c4dff; font-size: 13px;"
+);
