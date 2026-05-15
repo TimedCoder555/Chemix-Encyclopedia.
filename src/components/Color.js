cf7cbe5858f344b49b296868bd9d25629eccc4fd
TimedCoder555 <matrixebay0110@gmail.com>
2026-05-15 10:21:19 +0600

    Create Color.js

---
+++ src/components/Color.js
@@ -0,0 +1,209 @@
+module.exports = {
+  elements: [
+
+    /* Basic Elements */
+
+    {
+      name: "Hydrogen",
+      symbol: "H",
+      "cpk-hex": "#ffffff",
+    },
+
+    {
+      name: "Helium",
+      symbol: "He",
+      "cpk-hex": "#ff9ff3",
+    },
+
+    {
+      name: "Lithium",
+      symbol: "Li",
+      "cpk-hex": "#ff6b6b",
+    },
+
+    {
+      name: "Beryllium",
+      symbol: "Be",
+      "cpk-hex": "#48dbfb",
+    },
+
+    {
+      name: "Boron",
+      symbol: "B",
+      "cpk-hex": "#1dd1a1",
+    },
+
+    {
+      name: "Carbon",
+      symbol: "C",
+      "cpk-hex": "#576574",
+    },
+
+    {
+      name: "Nitrogen",
+      symbol: "N",
+      "cpk-hex": "#54a0ff",
+    },
+
+    {
+      name: "Oxygen",
+      symbol: "O",
+      "cpk-hex": "#ff4757",
+    },
+
+    {
+      name: "Fluorine",
+      symbol: "F",
+      "cpk-hex": "#2ed573",
+    },
+
+    {
+      name: "Neon",
+      symbol: "Ne",
+      "cpk-hex": "#e056fd",
+    },
+
+    {
+      name: "Sodium",
+      symbol: "Na",
+      "cpk-hex": "#ff9f43",
+    },
+
+    {
+      name: "Magnesium",
+      symbol: "Mg",
+      "cpk-hex": "#10ac84",
+    },
+
+    {
+      name: "Aluminium",
+      symbol: "Al",
+      "cpk-hex": "#8395a7",
+    },
+
+    {
+      name: "Silicon",
+      symbol: "Si",
+      "cpk-hex": "#feca57",
+    },
+
+    {
+      name: "Phosphorus",
+      symbol: "P",
+      "cpk-hex": "#ff9f1a",
+    },
+
+    {
+      name: "Sulfur",
+      symbol: "S",
+      "cpk-hex": "#f6e58d",
+    },
+
+    {
+      name: "Chlorine",
+      symbol: "Cl",
+      "cpk-hex": "#2ecc71",
+    },
+
+    {
+      name: "Argon",
+      symbol: "Ar",
+      "cpk-hex": "#9b59b6",
+    },
+
+    {
+      name: "Potassium",
+      symbol: "K",
+      "cpk-hex": "#ff6b81",
+    },
+
+    {
+      name: "Calcium",
+      symbol: "Ca",
+      "cpk-hex": "#c8d6e5",
+    },
+
+    /* Premium Elements */
+
+    {
+      name: "Iron",
+      symbol: "Fe",
+      "cpk-hex": "#ffa502",
+    },
+
+    {
+      name: "Copper",
+      symbol: "Cu",
+      "cpk-hex": "#c56c39",
+    },
+
+    {
+      name: "Silver",
+      symbol: "Ag",
+      "cpk-hex": "#dfe4ea",
+    },
+
+    {
+      name: "Gold",
+      symbol: "Au",
+      "cpk-hex": "#ffd700",
+    },
+
+    {
+      name: "Mercury",
+      symbol: "Hg",
+      "cpk-hex": "#a4b0be",
+    },
+
+    {
+      name: "Lead",
+      symbol: "Pb",
+      "cpk-hex": "#747d8c",
+    },
+
+    {
+      name: "Uranium",
+      symbol: "U",
+      "cpk-hex": "#00cec9",
+    },
+
+    {
+      name: "Plutonium",
+      symbol: "Pu",
+      "cpk-hex": "#00b894",
+    },
+
+    {
+      name: "Radon",
+      symbol: "Rn",
+      "cpk-hex": "#6c5ce7",
+    },
+
+    {
+      name: "Oganesson",
+      symbol: "Og",
+      "cpk-hex": "#fd79a8",
+    },
+
+    /* Extra Custom Chemix Theme */
+
+    {
+      name: "Chemix Crystal",
+      symbol: "Cx",
+      "cpk-hex": "#00e5ff",
+    },
+
+    {
+      name: "Timedium",
+      symbol: "TmX",
+      "cpk-hex": "#7c4dff",
+    },
+
+    {
+      name: "Nova Energy",
+      symbol: "Nv",
+      "cpk-hex": "#00ff99",
+    },
+
+  ],
+};
