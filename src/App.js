bda54abce44cb7d46db4a176dd28114be1ae7e44
TimedCoder555 <matrixebay0110@gmail.com>
2026-05-19 15:57:14 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
 import React from "react";
 import "./App.css";
 import Main from "./components/main";
-import logo from "./img/logo.png";
 
 function App() {
   return (
-    <section className="App-header">
-
-      {/* Logo */}
-      <img
-        alt="Chemix-Encyclopedia"
-        src={logo}
-        className="logo"
-        onClick={() => {
-          window.location.reload();
-        }}
-      />
-
-      {/* Title */}
-      <h1 className="title">
-        Chemix-Encyclopedia
-      </h1>
-
-      {/* Subtitle */}
-      <p className="subtitle">
-        Interactive Scientific Chemistry Experience
-      </p>
-
-      {/* Main Content */}
-      <div className="App">
-        <center>
-          <Main />
-        </center>
-      </div>
-
-    </section>
+    <div className="App">
+      <Main />
+    </div>
   );
 }
 
