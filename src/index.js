4eadc5a313d0f8eadd2085382a9d3dc3fcffc920
TimedCoder555 <matrixebay0110@gmail.com>
2026-05-19 15:57:47 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
-/* Loading Animation */
-
-const rootElement = document.getElementById("root");
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  rootElement
-);
-
-/* Performance Report */
-
-reportWebVitals();
-
-/* Console Message */
-
-console.log(
-  "%c Chemix-Encyclopedia Loaded Successfully 🚀",
-  "color: #00e5ff; font-size: 18px; font-weight: bold;"
-);
-
-console.log(
-  "%c Developed by timedcoder",
-  "color: #7c4dff; font-size: 14px;"
+  document.getElementById("root")
 );
